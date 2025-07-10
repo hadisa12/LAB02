4418f8ae03afd09dd737399f37ba0d1abdd7844f
Syeda Hadisa Nayab Rizvi <hadisa.rizvi@secp.gov.pk>
2025-07-10 10:52:10 +0500

    Success

---
+++ src/test/java/Lab01.java
@@ -0,0 +1,20 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Lab01 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password =
+                driver.findElement(By.id("password"));
+        WebElement LoginBTN = driver.findElement(By.id("loginbutton"));
+        username.sendKeys("standard_user");
+        password.sendKeys("secret_sauce");
+        LoginBTN.click();
+    }
+}
